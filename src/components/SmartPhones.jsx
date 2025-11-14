4d70f3c2238de75e550a97aadb4a3fc146b47e0c
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2025-11-14 12:18:29 +0530

    update

--- src/components/SmartPhones.jsx
+++ src/components/SmartPhones.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 const SmartPhones = ({ item }) => {
   const navigate = useNavigate();
 
+  if (!item) return null;
+
   const handleClick = () => {
     navigate(`/details/${item.id}`);
   };
@@ -14,7 +16,7 @@ const SmartPhones = ({ item }) => {
       className="group md:min-w-[160px] lg:min-w-[230px] flex-shrink-0"
       onClick={handleClick}
     >
-      <div className="py-[12px] mx-[30px] lg:mx-[0px] flex items-center md:flex-col">
+      <div className="!py-[12px] !mx-[30px] lg:mx-[0px!important] flex items-center md:flex-col">
         <div>
           <img
             src={item?.image || "/placeholder.png"}
@@ -22,19 +24,19 @@ const SmartPhones = ({ item }) => {
             className="h-[48px] md:h-[90px] lg:h-[132px] object-contain transition-transform lg:group-hover:scale-105"
           />
         </div>
-        <div className="w-full px-5 md:text-center xl:mb-[50px] md:mt-[12px]">
-          <h4 className="font-[500] md:font-[300] text-[13px] md:text-[13px] truncate">
+        <div className="w-full !px-5 md:text-center xl:mb-[50px!important] md:mt-[12px!important]">
+          <h1 className="font-[500] md:font-[400] text-[13px] md:text-[13px] truncate">
             {item?.title?.split("-")[0] || "No title"}
-          </h4>
+          </h1>
           <h1 className="text-gray-500 text-[13px] md:text-black md:font-semibold">
-            {item?.price}
+            {item?.price || "No price info"}
           </h1>
         </div>
-        <div className="p-[10px] md:hidden">
+        <div className="!p-[10px] md:hidden">
           <ChevronRight size={20} className="text-gray-500" />
         </div>
       </div>
-      <div className="bg-gray-300 mx-[8px] h-[1px] md:hidden"></div>
+      <div className="bg-gray-300 !mx-[8px] h-[1px] md:hidden"></div>
     </div>
   );
 };
