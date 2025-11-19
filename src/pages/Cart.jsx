5cf91da704b8cd1d064a7edbe032604d9caa6370
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2025-11-19 19:53:05 +0530

    cart finish

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -11,26 +11,29 @@ const Cart = () => {
   return (
     <section className="">
       {hasItems ? (
-        <div className="flex flex-col  gap-1 p-5">
-          {/* adress box */}
-          <div className="flex justify-between items-center bg-white ">
-            <h5 className=" font-semibold text-[12px] p-4 lg:text-[16px] lg:p-5 ">
-              From Saved Addresses
-            </h5>
-            <h6 className="text-[12px] font-semibold text-gray-600 border-[1px] border-gray-400 flex items-center p-2 mr-4">
-              Enter Delivery Pincode
-            </h6>
-          </div>
-          {/* cart */}
-          <div className="flex justify-center flex-col items-center md:flex-row md:items-start  gap-3.5 ">
-            {" "}
-            <div className="bg-white max-w-[700px]">
-              {cart.map((item) => (
-                <CartProductCard key={item.id} item={item} />
-              ))}
+        <div className="flex flex-col   gap-1 p-5">
+          <div className="flex flex-col md:flex-row md:justify-center md:items-start  gap-3.5  ">
+            {/* cart  */}
+            <div className="space-y-1">
+              {/* adress */}
+              <div className="flex justify-between items-center bg-white  ">
+                <h5 className=" font-semibold text-[12px] p-4 lg:text-[16px] lg:p-5 ">
+                  From Saved Addresses
+                </h5>
+                <h6 className="text-[12px] font-semibold text-gray-600 border-[1px] border-gray-400 flex items-center p-2 mr-4">
+                  Enter Delivery Pincode
+                </h6>
+              </div>
+              {/*cart box */}
+              <div className="bg-white max-w-[700px] ">
+                {cart.map((item) => (
+                  <CartProductCard key={item.id} item={item} />
+                ))}
+              </div>
             </div>
+
             {/* price box */}
-            <div className="flex flex-col gap-5 min-w-[300px] ">
+            <div className="flex flex-col gap-5 sm:min-w-[300px] ">
               {/*price details */}
               <div className="bg-white p-5">
                 <h1 className=" text-gray-400 ">PRICE DETAILS</h1>
